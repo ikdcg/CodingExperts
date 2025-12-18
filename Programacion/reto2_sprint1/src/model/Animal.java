b041d248eb137d0b0709b1b4422e244e0d12e52c
ikdcd <ikdcd@plaiaundi.net>
2025-12-18 14:13:49 +0100

    Ultimos cambios para el sprint 1

--- Programacion/reto2_sprint1/src/model/Animal.java
+++ Programacion/reto2_sprint1/src/model/Animal.java
@@ -3,31 +3,25 @@ package model;
 public class Animal {
 	// Atributos
 	private String name;
-	private String type;
 	private int age;
 	private String breed;
-	private String medHist;
 	private String caracteristics;
 	private String specialNeeds;
 	
 	// Constructor 1
-	public Animal(String name, String type, int age, String breed) {
+	public Animal(String name, int age, String breed) {
 		this.name = name;
-		this.type = type;
 		this.age = age;
 		this.breed = breed;
 		// Por defecto
-		this.medHist = "No hay historial medico.";
 		this.caracteristics = "No se ha detallado caracteristicas.";
 		this.specialNeeds = "No se ha detallado necesidades especiales.";
 	}
 	// Constructor 2
-	public Animal(String name, String type, int age, String breed, String medicalHistory, String caracteristics, String specialNeeds) {
+	public Animal(String name, int age, String breed, String caracteristics, String specialNeeds) {
 		this.name = name;
-		this.type = type;
 		this.age = age;
 		this.breed = breed;
-		this.medHist = medicalHistory;
 		this.caracteristics = caracteristics;
 		this.specialNeeds = specialNeeds;
 	}
@@ -39,18 +33,6 @@ public class Animal {
 
 	public void setAge(int age) {
 		this.age = age;
-	}
-	
-	public String getType() {
-		return type;
-	}
-
-	public String getMedicalHistory() {
-		return medHist;
-	}
-
-	public void setMedicalHistory(String medicalHistory) {
-		this.medHist = medicalHistory;
 	}
 
 	public String getCaracteristics() {
@@ -81,11 +63,8 @@ public class Animal {
 	@Override
 	public String toString() {
 		String result = "Nombre: "+this.name
-				+"\nTipo de animal: "+this.type
 				+"\nEdad: "+this.age
 				+"\nRaza: "+this.breed
-				+"\nHistorial medico:\n"
-				+this.medHist
 				+"\nCaracteristicas:\n"
 				+this.caracteristics
 				+"\nNecesidades especiales:\n"
